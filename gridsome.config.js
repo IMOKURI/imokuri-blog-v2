308d91d5fcbd51e3d77b6df20d3437aacb871eff
IMOKURI <nenegi.01mo@gmail.com>
2019-12-01 20:18:51 +0900

    add eslint

--- gridsome.config.js
+++ gridsome.config.js
@@ -32,7 +32,7 @@ module.exports = {
   ],
 
   transformers: {
-    //Add markdown support to all file-system sources
+    // Add markdown support to all file-system sources
     remark: {
       externalLinksTarget: '_blank',
       externalLinksRel: ['nofollow', 'noopener', 'noreferrer'],
